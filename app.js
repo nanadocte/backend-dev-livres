e1410787d4cabefda3a7c6303884e68fee605d81
nanadocte <206342507+nanadocte@users.noreply.github.com>
2026-01-21 21:13:48 +0100

    Configuration du token et du middleware d'authentification

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose');
 const bookRoutes = require('./routes/book')
+const userRoutes = require('./routes/user');
 const app = express()
 
 mongoose.connect('mongodb+srv://projetbook:projetbook@cluster0.ghzbosi.mongodb.net/?appName=Cluster0',)
@@ -22,6 +23,6 @@ app.use((req, res, next) => {
 
 
 app.use('/api/books', bookRoutes)
-
+app.use('/api/auth', userRoutes);
 
 module.exports = app
--- routes/book.js
+++ routes/book.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const bookCtrl = require('../controllers/book')
+const auth = require('../midlleware/auth')
 
 
-router.post('/', bookCtrl.createBook)
+router.post('/', auth, bookCtrl.createBook)
 
 router.get('/', bookCtrl.getAllBooks)
 
 router.get('/:id', bookCtrl.getOneBook)
 
-router.put('/:id', bookCtrl.modifyBook)
+router.put('/:id', auth, bookCtrl.modifyBook)
 
-router.delete('/:id', bookCtrl.deleteBook)
+router.delete('/:id', auth, bookCtrl.deleteBook)
 
 
 module.exports = router
