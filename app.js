94505346600d48b20f53b49f92e539016589c536
nanadocte <206342507+nanadocte@users.noreply.github.com>
2026-01-21 19:38:29 +0100

    Création de routes DELETE et PUT pour la suppression et la modification d'un book

--- app.js
+++ app.js
@@ -38,14 +38,32 @@ app.get('/api/books/:id', async (req, res, next)=> {
     }
 })
 
+app.put('/api/books/:id', async(req, res, next)=> {
+    try{
+        await Book.updateOne({ _id : req.params.id}, {...req.body, _id : req.params.id}) 
+        res.status(200).json({message : 'Livre modifié'})
+    }catch(error){
+        res.status(400).json({error})
+    }
+})
+
+app.delete('api/books/:id', async(req, res, next)=> {
+    try{
+        await Book.deleteOne({_id : req.params.id})
+        res.status(200).json({message : 'Livre supprimé'})
+    }catch(error){
+        res.status(400).json({error})
+    }
+})
+
 app.post('/api/books', async (req, res, next)=>{
-     try {
-    const {_id, ...data} = req.body
-    const book = new Book (data)
-    await book.save()
-    res.status(201).json({message : 'Objet enregistré'})
+    try {
+        const {_id, ...data} = req.body
+        const book = new Book (data)
+        await book.save()
+        res.status(201).json({message : 'Objet enregistré'})
   }catch(error){
-    res.status(400).json({error})
+        res.status(400).json({error})
 
 }})
 
